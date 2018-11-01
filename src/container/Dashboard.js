cfeb07362ca95044e06bf986bb9dcb2942f260b0
Muhammad Fahmi Irfananda <fahmiirfan909@gmail.com>
2018-11-01 15:39:19 +0700

    add horizontal padding

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -3,22 +3,42 @@ import React, { Component } from "react"
 import { Link } from "react-router-dom"
 
 import { withStyles } from '@material-ui/core/styles';
-import { Typography } from "@material-ui/core"
+import { Typography, createStyles } from "@material-ui/core"
 import Button from '@material-ui/core/Button'
 
 const scanLink = props => <Link to="/scan" {...props} />
 
+const styles = createStyles({
+  root: {
+    justify: "center",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
 class Dashboard extends Component {
   render() {
+    const { classes } = this.props
+
     return(
-      <React.Fragment>
+      <main className={classes.root}>
         <Typography variant="h4">Dashboard</Typography>
         <Button component={scanLink} variant="contained" color="primary" fullWidth={true}>
           Scan!
         </Button>
-      </React.Fragment>
+      </main>
     );
   }
 }
 
-export default withStyles()(Dashboard)
+export default withStyles(styles)(Dashboard)
